4f97162c4aff38352257b68f83e8780a617e0c49
thestackdev <shanmukeshwar03@gmail.com>
2024-05-02 16:25:56 +0530

    Package updates

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  presets: ["module:@react-native/babel-preset"],
+module.exports = function (api) {
+  if (api) {
+    api.cache(false);
+  }
+
+  return {
+    presets: ["module:metro-react-native-babel-preset"],
+  };
 };
